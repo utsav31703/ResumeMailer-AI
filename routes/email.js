1deaa6ffb914d182d17d87d2aff980aec5e6a4b0
Utsav <utsav317@gmail.com>
2025-08-31 16:35:14 +0530

    added csv feature and sending mails with it

--- routes/email.js
+++ routes/email.js
@@ -1,8 +1,12 @@
 import express from "express"
 import {google} from "googleapis"
+import multer from "multer";
+import fs from "fs";
+import csv from "csv-parser"
 
 const router =express.Router();
 
+const upload =multer({dest:"uploads/"})
 //Middleware : check if user is logged in
 
 function isLoggedIn(req,res,next){
@@ -55,4 +59,53 @@ router.post("/send-bulk",isLoggedIn,async(req,res)=>{
   }
 
 })
+
+router.post("/upload-csv",isLoggedIn,upload.single("csvfile"),async (req,res)=>{
+    try {
+        if(!req.file){
+             return res.status(400).json({ error: "No CSV file uploaded" });
+        }
+        const contacts=[];
+        
+          // Parse CSV
+          fs.createReadStream(req.file.path).pipe(csv()).on("data",(row)=>{
+            if(row.email){
+                contacts.push({name:row.name,email:row.email});
+            }
+          }).on("end",async ()=>{
+            console.log("✅ CSV parsed:", contacts);
+
+            if(contacts.length===0){
+                return res.status(400).json({error:"CSV has no vaild emails"});
+            }
+
+            //Delete file after parsing
+            fs.unlinkSync(req.file.path);
+
+            //prepare email data
+            const to=contacts.map((c)=>c.email);
+            const subject="Test Bulk Email";
+            const message="Hello, this is a test buld email from the ResumeMailer";
+
+             // Call your existing bulk email logic
+             req.body={to,subject,message};
+
+              // 👇 Instead of duplicating, call send-bulk directly
+              router.handle(
+                {...req,url:"/send-bulk",method:"POST"},
+                res,
+                (err)=>{
+                    if(err) console.error("Error forwarding to send-bulk:", err);
+                    
+                }
+              )
+          })
+          
+
+    } catch (error) {
+        console.error("❌ Error uploading CSV:", err);
+    res.status(500).json({ error: "Failed to process CSV" });
+    }
+})
+
 export default router;
