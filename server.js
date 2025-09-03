15e357df7433874f7627354384df72bf7e95b185
Utsav kushwaha <94130476+utsav31703@users.noreply.github.com>
2025-09-03 14:34:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -53,8 +53,8 @@ app.use((err, req, res, next) => {
   console.error("❌ Server Error:", err);
   res.status(500).json({ error: "Internal server error" });
 });
-const PORT=process.env.PORT
-app.listen(PORT,()=>{
+const PORT=process.env.PORT ||5000;
+app.listen(PORT,"0.0.0.0",()=>{
     console.log(`🚀 Server running on http://localhost:${PORT}`);
     
 })
